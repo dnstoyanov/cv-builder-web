edb841f1b9955bce3ee93d9d8330e26f8d29df69
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2024-03-01 18:05:44 +0200

    add some text for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 const App = () => {
-  return (
-    <div>
-      <h1 className="font-bold text-xl text-center text-orange-500">
-        CV Builder
-      </h1>
+    return (
+        <div>
+            <h1 className="font-bold text-xl text-center text-orange-500">
+                CV Builder
+            </h1>
 
-      <h2 className="font-regular text-red-600 text-center">
-        Testing Tailwind and Vite Just for the test
-      </h2>
+            <h2 className="font-regular text-red-600 text-center">
+                Testing Tailwind and Vite Just for the test
+            </h2>
 
-      <h2 className="font-regular text-red-600 text-center">
-        Starting new project with Vite and React
-      </h2>
-    </div>
-  );
+            <h2 className="font-regular text-red-600 text-center">
+                Starting new project
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+                sint!
+            </p>
+        </div>
+    );
 };
 
 export default App;
