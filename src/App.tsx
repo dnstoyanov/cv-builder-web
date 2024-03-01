16e87f60daaa384b382724c7fd3cb3b89be94d93
@d.stoyanov <d.stoyanov@celtis.net>
2024-03-01 17:32:15 +0200

    testing addons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 const App = () => {
-    return (
-        <div>
-            <h1 className="font-bold text-xl text-center text-orange-500">
-                CV Builder
-            </h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="font-bold text-xl text-center text-orange-500">
+        CV Builder
+      </h1>
+
+      <h2 className="font-regular text-red-600 text-center">
+        Testing Tailwind and Vite
+      </h2>
+    </div>
+  );
 };
 
 export default App;
