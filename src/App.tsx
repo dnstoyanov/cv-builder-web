a270e9ac3269121a45a0afc01a87a9bb5ffc87e2
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2024-03-04 14:32:22 +0200

    reinit project to NextJS

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { RouterProvider } from 'react-router-dom';
-import router from './config/router';
-
-const App = () => {
-    return <RouterProvider router={router} />;
-};
-
-export default App;
