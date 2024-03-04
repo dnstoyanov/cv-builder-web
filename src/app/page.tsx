5361ed29d4fa37e1ba2a07afbff3365fb0fd956c
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2024-03-04 14:51:07 +0200

    feat: add material ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Avatar } from '@mui/material';
+
 export default function Home() {
     return (
-        <section>
-            <h1>CV Builder App</h1>
+        <section className='flex justify-center'>
+            <h1 className='font-bold text-orange-500'>CV Builder App</h1>
+            <Avatar
+                alt='demo user'
+                src='https://images.pexels.com/photos/7098048/pexels-photo-7098048.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            />
         </section>
     );
 }
